60ecbd1cd9b80f6a33c2f5c28e279c2359ce423c
hiepnq <hiepnq@TS24-CHUNGNT1>
2026-04-13 15:44:52 +0700

    13/04/26 lần 2

--- Ajax_Form_Post_MVC/Class/Utility.cs
+++ Ajax_Form_Post_MVC/Class/Utility.cs
@@ -2100,7 +2100,7 @@ namespace MVC_QuanLyTHP.Class
 					{
 						if (Type.GetTypeCode(value.GetType()).ToString().Contains("Int"))
 						{
-							valueEdit.Value = ((int)value).ToString("N0");
+							valueEdit.Value = ((int)value).ToString("N0").Replace(".", ",");
 						}
 						else if (value.ToString().Contains(","))
 						{
@@ -2109,7 +2109,7 @@ namespace MVC_QuanLyTHP.Class
 						}
 						else
 						{
-							valueEdit.Value = ((double)value).ToString("N0");
+							valueEdit.Value = ((double)value).ToString("N0").Replace(".", ",");
 						}
 					}
 					else
